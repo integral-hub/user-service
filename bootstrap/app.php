5eb7b35b1a4401ce3c7f0a883859e6819bfb8b6f
Abiodun Adeosun <hadexemmanuela@gmail.com>
2025-11-12 17:59:47 +0100

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
-            HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
